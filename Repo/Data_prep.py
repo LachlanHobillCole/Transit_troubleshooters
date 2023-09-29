f73e90f01f3505727416bd479e6b59253b7992dc
RlGillman <rgillman@kpmg.com.au>
2023-09-29 11:32:37 +1000

    Update Data_prep.py
    adding bussfleet class

--- Repo/Data_prep.py
+++ Repo/Data_prep.py
@@ -2,51 +2,6 @@ import os
 import pandas as pd
 import numpy as np
 
-# def read_text_files_to_dataframes(data_path):
-#     # Initialise an empty dictionary to store DataFrames
-#     dataframes = {}
-#     pd.set_option('display.max_columns',None)
-
-#     # Loop through all files in the folder
-#     for filename in os.listdir(data_path):
-#         if filename.endswith(".txt"):  # reading text files
-#             file_path = os.path.join(data_path, filename)
-            
-#             # Read the text file into a DataFrame
-#             df = pd.read_csv(file_path, sep=',')  # Can adjust the separator
-            
-            
-#             # Use the file name (without extension) as the DataFrame name
-#             dataframe_name = os.path.splitext(filename)[0]
-            
-#             # Store the DataFrame in the dictionary with the file name as the key
-#             dataframes[dataframe_name] = df
-
-#     return dataframes
-
-
-# def read_csv_files_to_dataframes(data_path):
-#     # Initialise an empty dictionary to store DataFrames
-#     csv_dataframes = {}
-#     pd.set_option('display.max_columns',None)
-
-#     # Loop through all files in the folder
-#     for filename in os.listdir(data_path):
-#         if filename.endswith('.csv'):  # reading text files
-#             file_path = os.path.join(data_path, filename)
-            
-#             # Read the text file into a DataFrame
-#             df = pd.read_csv(file_path)  # Can adjust the separator
-            
-            
-#             # Use the file name (without extension) as the DataFrame name
-#             csv_dataframe_name = os.path.splitext(filename)[0]
-            
-#             # Store the DataFrame in the dictionary with the file name as the key
-#             csv_dataframes[csv_dataframe_name] = df
-
-#     return csv_dataframes
-
 
 def read_files_to_dataframes(data_path):
     # Initialize an empty dictionary to store DataFrames
@@ -71,3 +26,17 @@ def read_files_to_dataframes(data_path):
             dataframes[dataframe_name] = df
 
     return dataframes
+
+# Define the BusFleetData class
+class BusFleetData:
+    def __init__(self):
+        self.bus_data = []
+
+    def get_bus_info(self, bus_id):
+        # Retrieve information about a specific bus
+        for bus in self.bus_data:
+            if bus['bus_id'] == bus_id:
+                return bus
+            
+
+
